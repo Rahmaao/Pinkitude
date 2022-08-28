e1e8e264583bff0187e90dd19e3812f424a39cc1
Femi Olatubosun <thefemiolatubosun@gmail.com>
2022-08-28 18:04:17 +0100

    Added donate, upcoming, stories pages

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,11 @@
 const sidebar = document.querySelector("#jsSidebar");
 const burger = document.querySelector("#jsBurgerButton")
 const close = document.querySelector("#jsCloseButton")
+const toggleSidebarDrop = document.querySelector("#dropable-sidebar-link");
 
+toggleSidebarDrop.addEventListener("click", () => {
+    toggleSidebarDrop.classList.toggle("show-dropmenu")
+ })
 
 
 burger.addEventListener("click", function(){
@@ -10,3 +14,24 @@ burger.addEventListener("click", function(){
 close.addEventListener("click", function(){
     sidebar.classList.remove("pinkitude-navbar__sidebar_show")
 })
+
+
+$('.portfolio-menu ul li').click(function(){
+    $('.portfolio-menu ul li').removeClass('active');
+    $(this).addClass('active');
+    
+    var selector = $(this).attr('data-filter');
+    $('.portfolio-item').isotope({
+        filter:selector
+    });
+    return  false;
+});
+$(document).ready(function() {
+var popup_btn = $('.popup-btn');
+popup_btn.magnificPopup({
+type : 'image',
+gallery : {
+    enabled : true
+}
+});
+});
